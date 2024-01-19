1eb3729603b0a8feaf5fbad76011140d31f4a06f
Ayush Patange <patangeayush998@gmail.com>
2024-01-19 19:34:57 +0530

    Update Associate_Professer.java

--- Associate_Professer.java
+++ Associate_Professer.java
@@ -1,30 +1,45 @@
 package employee;
 
+//This is child class named as AssociateProfessor. It has Abstract methods inherited from Employee.
+
 public class Associate_Professer extends Employee{
+
+	// Method To calculate DA its takes Basic_pay as a input paramter.
+	
 	public double calculateDA(double Basic_pay) {
 		double DA = Basic_pay * 97/100;
 		return DA;
 	}
+
+	// Method To calculate HRA its takes Basic_pay as a input paramter.
 	
 	public double calculateHRA(double Basic_pay) {
 		double HRA = Basic_pay * 10/100;
 		return HRA;
 	}
+
+	// Method To calculate PF its takes Basic_pay as a input paramter.
 	
 	public double calculatePF(double Basic_pay) {
 		double PF = Basic_pay * 12/100;
 		return PF;
 	}
+
+	// Method To calculate SCF its takes Basic_pay as a input paramter.
 	
 	public double calculateSCF(double Basic_pay) {
 		double PF = Basic_pay * 0.1/100;
 		return PF;
 	}
+
+	// Method To calculate Gross Salary its takes Basic_pay as a input paramter.
 	
 	public double calculateGS(double bp4, double da4, double hra4) {
 		double GS = bp4 + da4 + hra4;
 		return GS;
 	}
+
+	// Method To calculate Net Salary its takes Basic_pay as a input paramter.
 	
 	public double calculateNS(double gs4, double pf4, double scf4) {
 		double NS = gs4- pf4- scf4;
